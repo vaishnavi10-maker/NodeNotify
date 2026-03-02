f8a1a2ad87d3308f9d21379196ddf6d31d027cab
vaishnavi <154087914+vaishnavi10-maker@users.noreply.github.com>
2026-03-02 22:56:50 +0530

    env file update

--- front/src/api/dashboardApi.js
+++ front/src/api/dashboardApi.js
@@ -3,5 +3,6 @@ import API from "./api";
 export const getUser = () => API.get("/user/me");
 
 export const getTasks = () => API.get("/tasks");
+export const getProblems = () => API.get("/problems");
 
 export const completeTask = (id) => API.patch(`/tasks/${id}`);
